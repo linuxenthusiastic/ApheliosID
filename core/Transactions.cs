4e7e8618cff3335d9553301fd7b183f31cdd7faa
zodiacsilence <mbitakrec@gmail.com>
2025-11-10 21:21:51 -0400

    Taking into account the selected elements, the body of the transactions allows us to visualize what we can do by implementing solid principles and software design.

--- core/Transactions.cs
+++ core/Transactions.cs
@@ -9,8 +9,32 @@ class Transaction
     public string Data {get;}
     public DateTime Timestamp {get;}
     public string Signature {get;}
-
+    public string Hash{get;}
     
 
+    public Transaction(string from , TransactionType type, string data)
+    {
+            if (string.IsNullOrWhiteSpace(from))
+                throw new ArgumentException("El campo 'From' no puede estar vacío", nameof(from));
+            
+            if (string.IsNullOrWhiteSpace(data))
+                throw new ArgumentException("El campo 'Data' no puede estar vacío", nameof(data));
 
+            From = from;
+            Type = type;
+            Data = data;
+            Timestamp = DateTime.UtcNow;
+            Hash = CalculateHash();
+    }
+
+    public Transaction(string from, TransactionType type, string data, 
+                          DateTime timestamp, string signature, string hash)
+    {
+            From = from;
+            Type = type;
+            Data = data;
+            Timestamp = timestamp;
+            Signature = signature;
+            Hash = hash;
+    }
 }
