29ddcdc25bc9429be3f264a916a18b29c40f7e2b
zodiacsilence <mbitakrec@gmail.com>
2025-11-11 22:03:38 -0400

    The transaction class model is finalized by applying advanced asymmetric RSA encryption concepts, verifying and signing, and also marking the types of transactions that will exist at the moment

--- core/Transactions.cs
+++ core/Transactions.cs
@@ -1,16 +1,19 @@
 using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
 
 namespace ApheliosID.Core;
 
 class Transaction
 {
-    public string From {get;}
-    public string To {get;}
-    public string Data {get;}
-    public DateTime Timestamp {get;}
-    public string Signature {get;}
-    public string Hash{get;}
-    
+    public readonly string From;
+    public readonly string To;
+    public readonly string Data;
+    public readonly DateTime Timestamp;
+    public readonly string Signature;
+    public readonly string Hash;
+    public readonly TransactionType Type;
 
     public Transaction(string from , TransactionType type, string data)
     {
@@ -55,4 +58,73 @@ class Transaction
     {
         return Hash == CalculateHash();
     }
+
+    public bool VerifySignature(string publickey)
+    {
+	    if(string.IsNullOrWhiteSpace(Signature))
+		    throw new InvalidOperationException("La transacción no tiene una firma para verificar.");
+	    try
+	    {
+		    byte[] signatureBytes = Convert.FromBase64String(Signature);
+		    byte[] dataBytes = Enconding.UTF8.GetBytes($"{From}{Type]{Data}{Timestamp:0}");
+		    using (RSA rsa = RSA.Create())
+		    {
+			    rsa.FromXmlString(publickey);
+			    return rsa.VerifyData(dataBytes, 
+					    signatureBytes, 
+					    HashAlgorithmName.SHA256, 
+					    RSASignaturePadding.Pkcs1);
+		    }
+	    }
+	    catch (System.Exception)
+	    {
+	        Console.WriteLine("Error al verificar la firma de la transacción.");
+		return false;
+	    }
+    }
+
+    public void Sign(string privateKeyPem)
+    {
+	try
+	{
+	    byte[] dataBytes = Encoding.UTF8.GetBytes($"{From}{Type}{Data}{Timestamp:O}");
+	    using (RSA rsa = RSA.Create())
+	    {
+		    rsa.FromXmlString(privateKeyPem);
+		    byte[] signatureBytes = rsa.SignData(dataBytes, 
+				    HashAlgorithmName.SHA256, 
+				    RSASignaturePadding.Pkcs1);
+		    Signature = Convert.ToBase64String(signatureBytes);
+	    }
+	}
+	catch (System.Exception)
+	{
+		throw new InvalidOperationException("Error al firmar la transacción.");
+	}
+    }
+
+    public bool IsValid(string publicKeyPem)
+    {
+	    if(!HasValidHash())
+	    {
+		    Console.WriteLine("Hash X.");
+		    return false;
+	    }
+	    if(!VerifySignature(publicKeyPem))
+	    {
+		    Console.WriteLine("Firma X");
+		    return false;
+	    }
+	    return true;
+    }
+}
+
+public enum TransactionType
+{
+	CREATE_IDENTITY,
+	UPDATE_IDENTITY,
+	DELETE_IDENTITY,
+	ISSUE_CREDENTIAL,
+	REVOKE_CREDENTIAL,
+	DEACTIVATE_IDENTITY
 }
